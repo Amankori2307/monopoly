34397be8989f17d71f0de4c9b4c7411d59ed8b98
amankori2307 <amankori2307@gmail.com>
2021-08-15 16:24:40 +0530

    Added Data For Minish Woods

--- src/components/modal/CardModal.js
+++ src/components/modal/CardModal.js
@@ -1,11 +1,10 @@
 import style from '../../assets/css/modal.module.css'
+
 const CardModal = ({onClick}) => {
-
-
     return (
         <div className={style.cardModal} onClick={onClick}>
             <div className={style.card}>
-                
+                <p className={style.name}>{}</p>                
             </div>
         </div>
     ); 
