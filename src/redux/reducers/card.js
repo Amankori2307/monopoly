b09e3ea249292926f538458a620cba2ba6e6b602
amankori2307 <amankori2307@gmail.com>
2021-08-19 09:04:51 +0530

    Added Card Data

--- src/redux/reducers/card.js
+++ src/redux/reducers/card.js
@@ -1,19 +1,8 @@
 import {SET_CURRENT_CARD, SET_SHOW_MODAL} from '../actions/actionTypes'
 
 const initialState = {
-    showModal: true,
-    currentCard: {
-        "id": 2,
-        "type": "site",
-        "color": "brown",
-        "textColorOnShow": "white",
-        "name": "Minish Woods",
-        "sellingPrice": 60,
-        "rent": 2,
-        "mortgage": 30,
-        "construction": 50,
-        "rentWithHouse": [ 10, 30, 90, 160, 250 ]
-    }
+    showModal: false,
+    currentCard: {}
 }
 
 export default function cardReducer(state = initialState, action) {
