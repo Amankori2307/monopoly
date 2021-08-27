d29f4206f15ad7ee58f78b9779f5b81ad4419aac
amankori2307 <amankori2307@gmail.com>
2021-08-27 06:32:33 +0530

    Added a logic to disable redux devtool on prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,13 @@ import {createStore} from "redux";
 import rootReducer from "./reducers/rootReducer";
 
 export default function configureStore(){
+    console.log(process.env.NODE_ENV)
     return createStore(
         rootReducer,
         // applyMiddleware(
             // thunk,
             
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        process.env.NODE_ENV==="development"?window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__():""
         // )
     )
 }
