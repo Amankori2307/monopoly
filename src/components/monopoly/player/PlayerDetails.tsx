e594b6d8f161b4c6d762e74fc3a6fc3289fb8c57
Your Name <you@example.com>
2023-02-27 00:39:46 +0530

    Refactored PlayerDetails component

--- src/components/monopoly/player/PlayerDetails.tsx
+++ src/components/monopoly/player/PlayerDetails.tsx
@@ -1,41 +1,49 @@
-import { connect } from 'react-redux';
-import style from '../../../assets/css/player-details.module.scss'
-import { setShowModal, setPlayerIdForMyCardsModal } from '../../../redux/actions/modal'
-import { modalTypes, colors } from '../../../utility/constants';
-const PlayerDetails = ({ playersData, playerId, setShowModal, setPlayerIdForMyCardsModal }) => {
-    const player = playersData.players[playerId]
-    const active = (playersData.activePlayer === playerId)
-    const color = colors[playerId]
-    const viewMyCards = () => {
-        setShowModal(true, modalTypes.MY_CARDS)
-        setPlayerIdForMyCardsModal(player.playerId)
-    }
+import { useDispatch, useSelector } from 'react-redux';
+import style from '../../../assets/css/player-details.module.scss';
+import {
+  setPlayerIdForMyCardsModal,
+  setShowModal,
+} from '../../../redux/actions/modal';
+import { IState } from '../../../redux/reducers/rootReducer';
+import { colors, modalTypes } from '../../../utility/constants';
 
-    return (
-        <div className={`${style.playerDetails} ${style[color]} ${active ? style.active : ""}`}>
-            <div className={style.header}>
-                <p className={style.playerName}>Player {player.playerId}</p>
-                <div className={style.overley}></div>
-            </div>
-            <div className={style.details}>
-                <p className={style.playerMoney}><b>Money:</b> ${player.money}</p>
-                <button className={style.viewMyCards} onClick={viewMyCards} >Views My Cards</button>
-            </div>
-        </div>
-    );
-}
-const mapStateToProps = (store) => {
-    return {
-        playersData: store.playersData,
-        // activePlayer: store.playersData.activePlayer
-    }
+interface PlayerDetailsPropsType {
+  playerId: number;
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        setShowModal: (showModal, currentModal) => dispatch(setShowModal(showModal, currentModal)),
-        setPlayerIdForMyCardsModal: (playerId) => dispatch(setPlayerIdForMyCardsModal(playerId)),
-    }
-}
+const PlayerDetails = (props: PlayerDetailsPropsType) => {
+  const { playerId } = props;
+  const dispatch = useDispatch();
+  const playersData = useSelector((store: IState) => store.playersData);
 
-export default connect(mapStateToProps, mapDispatchToProps)(PlayerDetails)
+  const player = playersData.players[playerId];
+  const active = playersData.activePlayer === playerId;
+  const color = colors[playerId];
+
+  const viewMyCards = () => {
+    dispatch(setShowModal(true, modalTypes.MY_CARDS));
+    dispatch(setPlayerIdForMyCardsModal(player.playerId));
+  };
+
+  return (
+    <div
+      className={`${style.playerDetails} ${style[color]} ${
+        active ? style.active : ''
+      }`}
+    >
+      <div className={style.header}>
+        <p className={style.playerName}>Player {player.playerId}</p>
+        <div className={style.overley}></div>
+      </div>
+      <div className={style.details}>
+        <p className={style.playerMoney}>
+          <b>Money:</b> ${player.money}
+        </p>
+        <button className={style.viewMyCards} onClick={viewMyCards}>
+          Views My Cards
+        </button>
+      </div>
+    </div>
+  );
+};
+export default PlayerDetails;
--- src/components/monopoly/player/PlayerDetailsContainer.tsx
+++ src/components/monopoly/player/PlayerDetailsContainer.tsx
@@ -1,19 +1,19 @@
-import style from '../../../assets/css/player-details.module.scss'
+import { useSelector } from 'react-redux';
+import { IState } from 'src/redux/reducers/rootReducer';
+import style from '../../../assets/css/player-details.module.scss';
 import PlayerDetails from './PlayerDetails';
-import { connect } from 'react-redux';
 
-const PlayerDetailsContainer = ({ totalPlayers }) => {
-    return (
-        <div className={style.playerDetailsContainer}>
-            {Array.from(Array(totalPlayers).keys()).map((id) => <PlayerDetails key={id} playerId={id} />)}
-        </div>
-    );
-}
+const PlayerDetailsContainer = () => {
+  const totalPlayers = useSelector(
+    (store: IState) => store.playersData.totalPlayers
+  );
+  return (
+    <div className={style.playerDetailsContainer}>
+      {Array.from(Array(totalPlayers).keys()).map((id) => (
+        <PlayerDetails key={id} playerId={id} />
+      ))}
+    </div>
+  );
+};
 
-const mapStateToProps = (store) => {
-    return {
-        totalPlayers: store.playersData.totalPlayers,
-    }
-}
-
-export default connect(mapStateToProps)(PlayerDetailsContainer)
+export default PlayerDetailsContainer;
