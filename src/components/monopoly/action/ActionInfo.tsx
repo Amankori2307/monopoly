9d1d2bdabaef5763e350f738b0b957d3494511cd
Your Name <you@example.com>
2023-02-27 00:58:12 +0530

    Refactored ActionInfo componenet

--- src/components/monopoly/action/ActionInfo.tsx
+++ src/components/monopoly/action/ActionInfo.tsx
@@ -1,34 +1,26 @@
-import { connect } from 'react-redux';
-import { setAction } from '../../../redux/actions/action'
-import style from '../../../assets/css/action-info.module.scss'
-const ActionInfo = ({ setAction, actionData }) => {
-    const onClose = () => {
-        setAction(false, null)
-    }
-    return (
+import { useDispatch, useSelector } from 'react-redux';
+import style from '../../../assets/css/action-info.module.scss';
+import { setAction } from '../../../redux/actions/action';
+import { IState } from '../../../redux/reducers/rootReducer';
+const ActionInfo = () => {
+  const actionData = useSelector((store: IState) => store.actionData);
+  const dispatch = useDispatch();
+  const onClose = () => {
+    dispatch(setAction(false, null));
+  };
+  return (
+    <div className={style.actionInfo}>
+      <div className={style.header}>
+        <p className={style.title}>{actionData.currentAction}</p>
+      </div>
+      <div className={style.main}>
+        <p>To {actionData.currentAction}, tap on the highlighted card.</p>
+        <button className={style.close} onClick={onClose}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
 
-        <div className={style.actionInfo}>
-            <div className={style.header}>
-                <p className={style.title}>{actionData.currentAction}</p>
-            </div>
-            <div className={style.main}>
-                <p>To {actionData.currentAction}, tap on the highlighted card.</p>
-                <button className={style.close} onClick={onClose}>Close</button>
-            </div>
-        </div>
-    );
-}
-
-const mapStateToProps = (store) => {
-    return {
-        actionData: store.actionData
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        setAction: (active, currentAction) => dispatch(setAction(active, currentAction))
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ActionInfo)
+export default ActionInfo;
